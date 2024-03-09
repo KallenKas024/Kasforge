ac8e6fcd185378274b8c768dac40916ca17cb0c9
KallenKas024 <3367662181@qq.com>
2024-03-09 16:40:49 +0800

    202403091840_update_ui

--- src/module/Home/Home.jsx
+++ src/module/Home/Home.jsx
@@ -7,22 +7,39 @@ function Home() {
     await exit()
   }
   return <>
-    <div className='title' >
-      <h2 data-tauri-drag-region className="title_h2">KASFORGE</h2>
-      <div className='title_close_container'>
-        <span onClick={closeApp} className='close_text'>X</span>
+  <div class='cover'>
+      <div className='title' >
+        <h2 data-tauri-drag-region className="title_h2">KASFORGE</h2>
+        <div className='title_close_container'>
+          <span onClick={closeApp} className='close_text'>X</span>
+        </div>
       </div>
-    </div>
-    <div className='link'>
-      <input type="text" />
-      <button>打包</button>
-    </div>
-
-    <div className='grid_view'>
-      <div className="grid_view_item"></div>
-      <div className="grid_view_item"></div>
-      <div className="grid_view_item"></div>
-      <div className="grid_view_item"></div>
+      <div class='pack-wrapper'>
+        <div className='input-wrapper'>
+          <div class='input-data'>
+            <input type="text" required/>
+            <div class='underline'></div>
+            <label class='path_label'>
+              打包地址
+            </label>
+          </div>
+        </div>
+        <button class='confirm-button'>打包</button>
+      </div>
+      <div class='underline'></div>
+      <div class='btn-panel'>
+        <ul>
+          <button class='night-btn'>夜间模式</button>
+          <button class='config-btn'>配置文件</button>
+          <button class='browse-btn'>浏览插件</button>
+          <button class='reset-btn'>重置插件</button>
+        </ul>
+      </div>
+      <div className='grid_view'>
+        <div class='integration-package-list'>
+          <h3>整合包列表</h3>
+        </div>
+      </div>
     </div>
   </>
 }
